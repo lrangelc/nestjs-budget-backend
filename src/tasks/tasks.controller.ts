3c64850868f621f17f32872e68110eb9929c3da4
Luis Rangel Castro <luisrangelc@gmail.com>
2022-09-20 19:39:25 -0600

    add crud to tasks
    super cool

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { ITask } from './task.model';
+
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): ITask[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): ITask {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): ITask {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Patch('/:id')
+  updateTask(
+    @Param('id') id: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ): ITask {
+    return this.tasksService.updateTask(id, updateTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string) {
+    return this.tasksService.deleteTask(id);
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,68 @@
 import { Injectable } from '@nestjs/common';
-
+import { ITask, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: ITask[] = [];
+
+  getAllTasks(): ITask[] {
+    return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): ITask {
+    const { title, description } = createTaskDto;
+
+    const task: ITask = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  getTaskById(id: string): ITask {
+    return this.tasks.find((element) => element.id === id);
+  }
+
+  updateTask(id: string, updateTaskDto: UpdateTaskDto): ITask {
+    const index = this.tasks.findIndex((element) => element.id === id);
+
+    let task: ITask = this.tasks[index];
+
+    const { title, description } = updateTaskDto;
+
+    task = {
+      ...task,
+      title,
+      description,
+    };
+
+    this.tasks[index] = task;
+
+    return task;
+  }
+
+  deleteTask(id: string) {
+    const result = {
+      success: true,
+      message: '',
+    };
+
+    const index = this.tasks.findIndex((element) => element.id === id);
+
+    if (index === -1) {
+      result.success = false;
+      result.message = 'Record not found.';
+      return result;
+    }
+
+    this.tasks = this.tasks.filter((element) => element.id !== id);
+    return result;
+  }
+}
