f8d17e6d42928106ab2d631569d674eab70971f5
Luis Rangel Castro <luisrangelc@gmail.com>
2022-10-01 11:33:17 -0600

    feat: add jwt and passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,25 @@
+/* eslint-disable hexagonal-architecture/enforce */
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthController } from './infrastructure/auth.controller';
 import { AuthService } from './application/auth.service';
 import { User } from './domain/user.entity';
 import { UsersRepositoryService } from './domain/users.repository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [AuthService, UsersRepositoryService],
   controllers: [AuthController],
 })
